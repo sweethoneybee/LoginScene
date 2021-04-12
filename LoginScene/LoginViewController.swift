c71dd3f3014966aae22e8a8a425a2b593a339bd5
jsjphone8 <jsjphone8@gmail.com>
2021-04-12 21:26:30 +0900

    로그인 레이아웃 구성 - keyboardFrameEndUserInfoKey 를 활용해 키보드가 가리는 UI를 위로 밀어주었음

---
+++ LoginScene/LoginViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  LoginScene
+//
+//  Created by 정성훈 on 2021/04/12.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    private var keyboardShown: Bool = false;
+    private var logoInvisible: Bool = false;
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var userIdTextField: UITextField!
+    @IBOutlet weak var scrollViewBottom: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userIdLabelTop: NSLayoutConstraint!
+    @IBOutlet weak var versionInfoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow(_:)), name: UIWindow.keyboardWillShowNotification, object:     nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide(_:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+        let contentViewTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.tapContentView))
+        
+        self.contentView.isUserInteractionEnabled = true
+        self.contentView.addGestureRecognizer(contentViewTapRecognizer)
+        
+        self.userIdTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        // setupUI()
+    }
+
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrameBegin = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrameBeginRect = keyboardFrameBegin.cgRectValue
+        
+        if !self.keyboardShown {
+            let distanceFromBottom = self.view.frame.height - self.LoginButton.frame.maxY
+            if distanceFromBottom <= keyboardFrameBeginRect.size.height + 90 {
+                animateUI(keyboardFrameBeginRect.size.height)
+            }
+        }
+        self.keyboardShown = true
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if self.keyboardShown && logoInvisible {
+            animateUI(0)
+        }
+        self.keyboardShown = false
+    }
+    
+    @objc func tapContentView() {
+        if self.keyboardShown == true {
+            self.view.endEditing(true)
+        }
+    }
+    
+    func animateUI(_ scrollValue: CGFloat) {
+        self.view.layoutIfNeeded()
+        self.scrollViewBottom.constant = scrollValue
+        
+        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .easeOut) {
+            self.userIdLabelTop.constant = !self.keyboardShown ? 23 : 56
+            self.view.layoutIfNeeded()
+            self.versionInfoLabel.alpha = self.keyboardShown ? 1 : 0
+        }
+        animator.startAnimation()
+        logoInvisible = !logoInvisible
+    }
+    
+    
+
+}
+
